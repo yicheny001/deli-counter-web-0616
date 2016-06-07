1ea75efd3dd4d296e8d4c25cf588d007ecbe23ab
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-07 04:49:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    total = katz_deli.size-1
+    for i in 0..total
+      message += " #{i+1}. #{katz_deli[i]}"
+    end
+    puts message
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+end
